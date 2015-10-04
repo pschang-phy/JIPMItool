47f1536bccc30970952d848fd6f28fc4aaf3e334
Po-Sheng Chang <posheng.chang@insyde.com>
2015-10-04 23:30:10 +0800

    Add Util function
    	1. Add variety of printbuf function
    	2. Add ipmi checksum function
    
    Signed-off-by: Po-Sheng Chang <posheng.chang@insyde.com>

--- src/posheng/study/ipmi/Util.java
+++ src/posheng/study/ipmi/Util.java
@@ -1,14 +1,45 @@
 package posheng.study.ipmi;
 
 public final class Util {
+	public static void printbuf(byte[] buf, int offset, int length,
+			String desc) {
 
-	public static void printbuf(int[] buf, int offset, int length, String desc) {
-		System.out.printf("%s (%d)\n", desc, length);
-		for (int i = 0; i < length; i++) {
-			if (0 != i && 0 == (i & 0x0F))
-				System.out.println("");
-			System.out.printf(" 0x%02X", buf[i] & 0xFF);
+		if (offset < 0 || offset >= buf.length)
+			throw new ArrayIndexOutOfBoundsException(offset);
+
+		int sentinel = offset + length > buf.length ? buf.length
+				: offset + length;
+
+		String output = "";
+		if (null != desc)
+			output += String.format("%s (%d %s)\n", desc, sentinel - offset,
+					sentinel - offset > 1 ? "bytes" : "byte");
+
+		for (int i = offset, j = 1; i < sentinel; i++, j++) {
+			output += String.format("%02X", buf[i] & 0xFF);
+			output += (0 == (j & 0x0F)) ? "\n" : " ";
 		}
-		System.out.println("");
+
+		System.out.println(output);
+	}
+
+	public static void printbuf(byte[] buf, String desc) {
+		printbuf(buf, 0, buf.length, desc);
+	}
+
+	public static void printbuf(byte[] buf, int offset, int length) {
+		printbuf(buf, offset, length, null);
+	}
+
+	public static void printbuf(byte[] buf) {
+		printbuf(buf, 0, buf.length, null);
+	}
+
+	public static byte ipmiChecksum(byte[] data, int start, int end) {
+		int checksum = 0;
+		for (int i = start; i < end; i++)
+			checksum = (checksum + data[i]) & 0xFF;
+
+		return (byte) -checksum;
 	}
 }
